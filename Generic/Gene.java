333ed52dc0c5b6b66f5fd8f87bc406377632c144
BoyangWei <38055163+BoyangWei@users.noreply.github.com>
2018-05-09 18:41:08 +0800

    Update Gene.java

--- Generic/Gene.java
+++ Generic/Gene.java
@@ -1,19 +1,30 @@
 package Final;
 
+import java.util.Random;
+
 public class Gene {
 	public static final int up = 0;
 	public static final int left = 1;
 	public static final int down = 2;
 	public static final int right = 3;
+	private int gene;
+	public Gene() {
+		randomGene();
+	}
 	public Gene(int i) {
 		this.gene=i;
 	}
-	private int gene;
+	
 	public int getGene() {
 		return gene;
 	}
 	public void setGene(int gene) {
 		this.gene = gene;
+	}
+	
+	private void randomGene() {
+		Random r = new Random();
+		this.gene = r.nextInt(4);
 	}
 	
 	
